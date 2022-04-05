a28f9828db94799acdea8db2905e956897576abe
PlatinumKellerman <platinumm@ukr.net>
2022-04-05 16:53:08 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,17 @@
+const categoriesArray = document.querySelectorAll('.item');
+const numberOfCategories = document.querySelectorAll('.item').length;
+
+const countCategoriesEl = () => {
+  console.log(`Number of categories: ${numberOfCategories}`); 
+};
+
+const findCategoriesEl = () => {
+    for (const category of categoriesArray) {
+        console.log(`Category: ${category.querySelector('h2').textContent}`);
+        console.log(`Elements: ${category.querySelectorAll('li').length}`);
+    };
+};
+
+countCategoriesEl();    
+findCategoriesEl();
+
