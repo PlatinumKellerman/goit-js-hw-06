857c857e823768da671fc680896357de6127ee42
PlatinumKellerman <platinumm@ukr.net>
2022-04-09 14:07:10 +0300

    task-06 - done

---
+++ js/task-06.js
@@ -0,0 +1,16 @@
+const inputEl = document.querySelector('#validation-input');
+
+inputEl.addEventListener('input', onInput);
+
+function onInput(input) {
+    if (input.currentTarget.value.length === 6) {
+       
+        inputEl.classList.remove('invalid');
+        inputEl.classList.add('valid');
+     
+    } else {
+        inputEl.classList.remove('valid');
+        inputEl.classList.add('invalid');
+    }
+}
+
