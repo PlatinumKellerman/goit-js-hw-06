bcfeb6a3943f2bb9985170dcca00d32cec15459f
PlatinumKellerman <platinumm@ukr.net>
2022-04-09 12:23:32 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,9 @@
 const nameInputEl = document.querySelector('#name-input');
 const nameOutputEl = document.querySelector('#name-output');
 
-nameInputEl.addEventListener('input', (event) => { 
-    if (event.currentTarget.value !== "") {
-        nameOutputEl.textContent = event.currentTarget.value;
+nameInputEl.addEventListener('input', (onNewNameInput) => { 
+    if (onNewNameInput.currentTarget.value !== "") {
+        nameOutputEl.textContent = onNewNameInput.currentTarget.value;
     } else {
         nameOutputEl.textContent = "Anonymous";
     }
