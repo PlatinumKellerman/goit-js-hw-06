4ba4c7fdf1f513f5872b71fd92a9a171e0dee73b
PlatinumKellerman <platinumm@ukr.net>
2022-04-10 13:44:25 +0300

    task-09 - done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const divWidgetEl = document.querySelector('.widget');
+const buttonChangeColorEl = document.querySelector('.change-color');
+const bodyEl = divWidgetEl.parentNode;
+const colorNameEl = document.querySelector('.color');
+
+buttonChangeColorEl.addEventListener('click', onChangeColorButtonClick);
+
+function onChangeColorButtonClick() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorNameEl.textContent = getRandomHexColor();
 }
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
