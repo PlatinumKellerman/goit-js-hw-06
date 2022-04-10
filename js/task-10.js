dcfedf716acbedf5212b75664c85166e47abcda0
PlatinumKellerman <platinumm@ukr.net>
2022-04-10 14:52:42 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,38 @@
+const divBoxesEl = document.querySelector('#boxes');
+const divControlsEl = document.querySelector('#controls');
+const inputNumberEl = divControlsEl.firstElementChild;
+const createdButtonEl = inputNumberEl.nextElementSibling;
+const destroyedButtonEl = createdButtonEl.nextElementSibling;
+
+
+
+inputNumberEl.addEventListener('input', onGetDivsAmount);
+function onGetDivsAmount(input) {
+  input.currentTarget.value;
+}
+
+createdButtonEl.addEventListener('click', createBoxes);
+function createBoxes() {
+  for (let i = 1; i <= inputNumberEl.value; i += 1) {
+    const addNewDivs = document.createElement('div');
+    
+    // divBoxesEl.append('addNewDivs');
+    // console.log(addNewDivs);
+  }
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
